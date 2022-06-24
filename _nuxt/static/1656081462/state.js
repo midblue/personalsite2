545c38ac4371f1a123f123f41e2ab896c4c8ba97
midblue <midblue@users.noreply.github.com>
2022-06-24 14:37:57 +0000

    deploy: 9699040558209b4276807833780dac705abd18df

---
+++ _nuxt/static/1656081462/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656081462",layout:"default",error:a,state:{dev:false,mobile:b,winSize:[1200,1000],lightboxSrc:a,tooltip:a},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
