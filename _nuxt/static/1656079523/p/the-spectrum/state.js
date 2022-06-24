7efbc49cfa8410e8ef48f715bb4499b109bf24cd
midblue <midblue@users.noreply.github.com>
2022-06-24 14:05:42 +0000

    deploy: 4753dfc7fc454c4331a904e054f62d894b705a8b

---
+++ _nuxt/static/1656079523/p/the-spectrum/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656079523",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fp\u002Fthe-spectrum",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
