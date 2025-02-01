a84a0e7d85140819626a2c1b0df6a852d4396032
Jasper Stephenson <jasperstephenson@gmail.com>
2025-02-01 10:06:48 +0900

    back to just me

--- generate/copy_docs_to_keep.ts
+++ generate/copy_docs_to_keep.ts
@@ -1,5 +1,5 @@
 const fs = require('fs')
-import * as c from '../assets/common'
+// import * as c from '../assets/common'
 
 const source = './generate/docs_to_keep/'
 const dest = './dist/'
@@ -8,7 +8,6 @@ fs.readdirSync(source).forEach((fileName) => {
   fs.copyFileSync(source + fileName, dest + fileName)
 })
 
-// make CNAME file with c.config.CNAME as content
-fs.writeFileSync(dest + 'CNAME', c.config.CNAME)
+fs.writeFileSync(dest + 'CNAME', 'www.jasperstephenson.com')
 
-c.log('Copied additional files (CNAME, etc) into /dist')
+console.log('Copied additional files (CNAME, etc) into /dist')
